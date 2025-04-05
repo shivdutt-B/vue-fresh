7b647829802d0d3b9213f495a11d3764549376bc
shivdutt-B <shivdutt059@gmail.com>
2025-04-05 15:32:50 +0530

    add contact

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="home-page">
+    <HelloWorld msg="Welcome to Vue.js" />
+  </div>
+</template>
+
+<style scoped>
+.home-page {
+  padding: 20px 0;
+}
+</style>
